3521562159fba5c3eb4a0905fd6f6b45b596c6f3
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-10-10 14:03:10 +0545

    first commit

---
+++ blog/views/main_view.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from ..models import Blogs
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def home(request):           
+    blogs = Blogs.objects.all()
+    return render(request, 'main/home.html',{'blogs' : blogs}) 
+
+
+@login_required
+def create_blog(request): #create blog
+    if request.method == 'POST':
+        title = request.POST.get("title")
+        subtitle = request.POST.get("subtitle")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+        blog = Blogs(title=title, subtitle=subtitle, description=description, image=image, author=request.user)  
+        blog.save()
+        return redirect("home")
+    return render(request, 'main/create.html')
+
+
+
+
+def edit_blog(request):              #edit blog
+    return render(request, 'main/edit.html')
+
+def single_blog(request,blog_id):             #single blog
+    blog = get_object_or_404(Blogs,pk=blog_id);
+    return render(request, 'main/single_blog.html',{"blog":blog})
+
+
+
+
+@login_required
+def delete_blog(request,blog_id):
+    blog= get_object_or_404(Blogs,pk=blog_id)
+    if blog.author == request.user:
+        blog.delete()
+        return redirect('home')
+    else:
+        return redirect('blog_detail',blog_id=blog.id)
+    
+    
