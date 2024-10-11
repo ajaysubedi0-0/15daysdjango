4177e94bb9c9777d6b8691a4e5862e3f72f58192
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-10-11 17:30:25 +0545

    Prepare for deployment

--- cms/settings.py
+++ cms/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['one5daysdjango.onrender.com','localhost','127.0.0.1:8000','127.0.0.1']
 
