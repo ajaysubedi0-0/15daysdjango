3ce43c53fe50ba8da3c98d005df8a757fec28513
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-10-13 18:21:05 +0545

    logo image+Nav Update

--- cms/settings.py
+++ cms/settings.py
@@ -178,7 +178,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    'UPLOAD_PRESET': os.getenv('CLOUDINARY_UPLOAD_PRESET'),
+    #'UPLOAD_PRESET': os.getenv('CLOUDINARY_UPLOAD_PRESET'),
 }
 
 DEFAULT_FILE_STORAGE= 'cloudinary_storage.storage.MediaCloudinaryStorage'
