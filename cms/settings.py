ad8508650db33c69ac3df27c1b4e97e3ca95a81f
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-10-25 22:32:28 +0545

    Update settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['one5daysdjango.onrender.com','localhost','127.0.0.1:8000','127.0.0.1']
+ALLOWED_HOSTS = ['one5daysdjango.onrender.com','localhost','127.0.0.1:8000','127.0.0.1','www.ajaysubedi.com.np']
 
 
 
